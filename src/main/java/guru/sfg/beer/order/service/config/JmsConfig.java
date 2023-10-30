fa8522f81cc7618da9cf8cf11da1774b985a65b0
Haileab-TG <haileab.t.g@gmail.com>
2023-10-30 17:37:24 -0500

    Spring JMS client integrated with docker container artemis broker

---
+++ src/main/java/guru/sfg/beer/order/service/config/JmsConfig.java
@@ -0,0 +1,18 @@
+package guru.sfg.beer.order.service.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.jms.support.converter.MappingJackson2MessageConverter;
+import org.springframework.jms.support.converter.MessageConverter;
+import org.springframework.jms.support.converter.MessageType;
+import org.springframework.stereotype.Component;
+
+@Component
+public class JmsConfig {
+    @Bean
+    public MessageConverter messageConverter(){
+        MappingJackson2MessageConverter messageConverter = new MappingJackson2MessageConverter();
+        messageConverter.setTargetType(MessageType.TEXT);
+        messageConverter.setTypeIdPropertyName("_type");
+        return messageConverter;
+    }
+}
--- target/generated-sources/annotations/guru/sfg/beer/order/service/web/mappers/BeerOrderLineMapperImpl_.java
+++ target/generated-sources/annotations/guru/sfg/beer/order/service/web/mappers/BeerOrderLineMapperImpl_.java
@@ -9,7 +9,7 @@ import org.springframework.stereotype.Component;
 
 @Generated(
     value = "org.mapstruct.ap.MappingProcessor",
-    date = "2023-10-28T14:39:06-0500",
+    date = "2023-10-30T17:26:11-0500",
     comments = "version: 1.5.5.Final, compiler: javac, environment: Java 20.0.1 (Oracle Corporation)"
 )
 @Component
